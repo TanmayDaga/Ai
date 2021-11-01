8ec331cd89ff79c51835ee03fcffca47f5fd69c9
Tanmay Daga <tanmay06daga@gmail.com>
2021-11-01 20:18:45 +0530

    update predictheartfailure.js

---
+++ my-app/src/components/DismmisibleAlert.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DismmisibleAlert() {
+    return (
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            A simple danger alert—check it out!
+        </div>
+    )
+}
--- my-app/src/components/PredictHeartFailure.js
+++ my-app/src/components/PredictHeartFailure.js
@@ -1,47 +1,46 @@
 import React from 'react'
 import * as tf from '@tensorflow/tfjs';
 
-import '@tensorflow/tfjs-backend-webgl';
 
 export default function PredictHeartFailure(props) {
     var model;
-    tf.setBackend('webgl');
-    async function loadModel(){
-        model =  await tf.loadLayersModel('https://raw.githubusercontent.com/TanmayDaga/Ai/main/models/model.json')
+
+    async function loadModel() {
+        model = await tf.loadLayersModel('https://raw.githubusercontent.com/TanmayDaga/Ai/main/models/model.json')
         console.log("model loaded success")
-        console.log(model)
     }
     loadModel();
-   
-  
+
+
 
     const loadValues = (value) => {
-        if (value === props.male){
+        if (value === props.male) {
             return 1;
         }
-        if (value === props.female){
+        if (value === props.female) {
             return 0;
         }
-        return 1?value === props.yes:0;
+        return 1 ? value === props.yes : 0;
     }
-    function roundToX(num, X) {    
-        return +(Math.round(num + "e+"+X)  + "e-"+X);
+    function roundToX(num, X) {
+        return +(Math.round(num + "e+" + X) + "e-" + X);
     }
     const makePrediction = () => {
+
         var input_xs;
         input_xs = tf.tensor2d([
             [Number(props.age),
-             loadValues(props.anaemia),
-              loadValues(props.diabetes),
-              loadValues(props.bp),
-              loadValues(props.gender),
-              loadValues(props.smoking)
+            loadValues(props.anaemia),
+            loadValues(props.diabetes),
+            loadValues(props.bp),
+            loadValues(props.gender),
+            loadValues(props.smoking)
             ]
         ])
-        console.log(input_xs);
+
         var output = model.predict(input_xs)
         var outputData = output.dataSync();
-        document.getElementById("answer").value = "You have "+ String(roundToX(Number(outputData)*100,2)) +" % chances of heart failure"
+        document.getElementById("answer").value = "You have " + String(roundToX(Number(outputData) * 100, 2)) + " % chances of heart failure"
 
 
     }
@@ -51,25 +50,25 @@ export default function PredictHeartFailure(props) {
     }
 
     const handleAge = (event) => {
-        var regex= /^[0-9]+$/ ;
-        if (event.target.value === "" || event.target.value.match(regex)){
+        var regex = /^[0-9]+$/;
+        if (event.target.value === "" || (event.target.value.match(regex))) {
             props.setAge(event.target.value)
         }
-        
+
     }
     const handleAnaemia = (event) => {
-        event === props.yes?props.setAnaemia(props.yes):props.setAnaemia(props.no)
+        event === props.yes ? props.setAnaemia(props.yes) : props.setAnaemia(props.no)
     }
     const handleSmoking = (event) => {
-        event === props.yes?props.setSmoking(props.yes):props.setSmoking(props.no)
+        event === props.yes ? props.setSmoking(props.yes) : props.setSmoking(props.no)
     }
     const handleBp = (event) => {
-        event === props.yes?props.setBp(props.yes):props.setBp(props.no)
+        event === props.yes ? props.setBp(props.yes) : props.setBp(props.no)
     }
     const handleDiabetes = (event) => {
-        event === props.yes?props.setDiabetes(props.yes):props.setDiabetes(props.no)
+        event === props.yes ? props.setDiabetes(props.yes) : props.setDiabetes(props.no)
     }
-   
+
     return (
         <>
             {/* Gender */}
@@ -86,14 +85,14 @@ export default function PredictHeartFailure(props) {
                 </div>
                 <div className="form-check-inline">
                     <label htmlFor="femaleGenderRadioButton">Female</label>
-                    <input type="radio" name="genderRadioButton" id="femaleGenderRadioButton" onClick={() => handleGender(props.female)} />
+                    <input type="radio" name="genderRadioButton" id="femaleGenderRadioButton"  defaultChecked={true} onClick={() => handleGender(props.female)} />
                 </div>
             </div>
 
             {/* Age */}
             <div className="form-group container-fluid">
-                <label for="inputAge">Age</label>
-                <input type="email" className="form-control" id="inputAge" aria-describedby="emailHelp"  onChange={(e) => {handleAge(e)}} value={props.age} placeholder="Enter Age" />
+                <label htmlFor="inputAge">Age</label>
+                <input type="email" className="form-control" id="inputAge" aria-describedby="emailHelp" onChange={(e) => { handleAge(e) }} value={props.age} placeholder="Enter Age" />
             </div>
 
             {/* Anameia */}
@@ -142,7 +141,7 @@ export default function PredictHeartFailure(props) {
                 </div>
                 <div className="form-check form-check-inline">
                     <label htmlFor="noBpRadioButton" className="form-check-inline">No</label>
-                    <input type="radio" name="BpRadioButton" id="noBpRadioButton"  defaultChecked={true} onClick={() => handleBp(props.no)}></input>
+                    <input type="radio" name="BpRadioButton" id="noBpRadioButton" defaultChecked={true} onClick={() => handleBp(props.no)}></input>
                 </div>
             </div>
 
@@ -162,9 +161,9 @@ export default function PredictHeartFailure(props) {
                     <input type="radio" name="DiabetesRadioButton" id="noDiabetesRadioButton" defaultChecked={true} onClick={() => handleDiabetes(props.yes)}></input>
                 </div>
             </div>
-            <div className="btn-primary" onClick ={makePrediction}>Click</div>
+            <div className="btn-primary" onClick={makePrediction}>Click</div>
             <div>
-                <textarea id = "answer"/>
+                <textarea id="answer" />
             </div>
         </>
     )
